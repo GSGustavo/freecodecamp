4dbf024cb96f3fbd360d16bde1f73c524eea1f86
Gustavo Gonzaga da Silva <47232603+GSGustavo@users.noreply.github.com>
2025-07-06 14:42:32 -0400

    feat: file-metadata-microservice

--- back-end-development-and-apis/boilerplate-project-filemetadata-main/index.js
+++ back-end-development-and-apis/boilerplate-project-filemetadata-main/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer()
 
 var app = express();
 
@@ -8,13 +10,21 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
 
+    const upfile = req.file;
 
+    res.json({
+        "name": upfile.originalname,
+        "type": upfile.mimetype,
+        "size": upfile.size
+    });
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
